67a35fd5eddc9288c8e803ec27b4aec7f1df5d51
ato26 <royalato@gmail.com>
2015-12-06 20:33:59 +0000

    Added ability to create quotes from a form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,30 @@
 class QuotesController < ApplicationController
+    
     def index
+        @quotes = Quote.order(created_at: :desc)
+    end
+    
+    def show
+        @quote = Quote.find(params[:id])
+    end
+    
+    def new
+        @quote = Quote.new
+    end
+    
+    def create
+        @quote = Quote.new(quote_params)
+        
+        if @quote.save
+            redirect_to @quote, notice: "Quote successfully created"
+        else
+            render new
+        end
+    end
+    
+    private
+    
+    def quote_params
+        params.require(:quote).permit(:author, :body, :context)
     end
 end
